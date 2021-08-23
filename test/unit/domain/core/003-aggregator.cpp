5199dbe91110a2ad0c0182ada992dcdfdabaf6dc
Dauliac <dauliac@pm.me>
2021-08-23 16:56:34 +0200

    [🐛] Fix aggregator remove using tests

--- test/unit/domain/core/003-aggregator.cpp
+++ test/unit/domain/core/003-aggregator.cpp
@@ -26,11 +26,50 @@ SCENARIO("Core Aggregator should be fillable.") {
 					REQUIRE(result.is_success() == true);
 					REQUIRE(result.get_success()->get_value() == value_object_ptr);
 					REQUIRE(aggregator.is_inside(uuid) == true);
+					REQUIRE(aggregator.count(uuid) == 1);
 				}
 				AND_THEN("The value inside the aggregator must be the ineserted value.") {
 					const int* value_in = aggregator.at(uuid).get_success()->get_value();
 					REQUIRE(value_in == value_object_ptr);
 				}
+			}
+		}
+	}
+}
+
+SCENARIO("Core Aggregator should be removable when object is in.") {
+	GIVEN("Uuid and related object inserted into it.") {
+		const Uuid uuid = "1234567";
+		const int value_object = 1234;
+		const int* value_object_ptr = &value_object;
+		auto aggregator = Aggregator<int const>();
+		auto const result = aggregator.insert_or_assign(uuid, value_object_ptr);
+
+		WHEN("Value object in is removed.") {
+			auto const result = aggregator.remove(uuid);
+			THEN("The object should be sucessfully removed.") {
+				REQUIRE(result.is_success() == true);
+			}
+			AND_THEN("The value should not be inside aggregator.") {
+				REQUIRE(aggregator.is_inside(uuid) == false);
+				REQUIRE(aggregator.count(uuid) == 0);
+			}
+		}
+	}
+}
+
+SCENARIO("Core Aggregator should be not removable when object is not in.") {
+	GIVEN("Uuid without object not inserted into it.") {
+		const Uuid uuid = "1234567";
+		auto aggregator = Aggregator<int const>();
+
+		WHEN("Value object in is removed.") {
+			auto const result = aggregator.remove(uuid);
+			THEN("The result should failure.") {
+				REQUIRE(result.is_failure() == true);
+			}
+			AND_THEN("The value should not be inside aggregator.") {
+				REQUIRE(aggregator.is_inside(uuid) == false);
 			}
 		}
 	}
