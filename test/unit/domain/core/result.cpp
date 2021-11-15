f9075c6425641c7451db086a7074d6e6f64f5812
Dauliac <dauliac@pm.me>
2021-11-15 12:11:53 +0100

    [✅] Add tests for Result.set_success

--- test/unit/domain/core/result.cpp
+++ test/unit/domain/core/result.cpp
@@ -82,7 +82,7 @@ SCENARIO("Core Result helper should be usable as success by value") {
 	}
 }
 
-SCENARIO("Core Result helper should be usable as failure with one possibility.") {
+SCENARIO("Core Result helper should be usable as failure with one possibility") {
 	GIVEN("Invalid operation") {
 		int const* invalid_result = nullptr;
 		WHEN("Result is created") {
@@ -99,7 +99,7 @@ SCENARIO("Core Result helper should be usable as failure with one possibility.")
 	}
 }
 
-SCENARIO("Core Result helper should be usable as failure with some possibility.") {
+SCENARIO("Core Result helper should be usable as failure with some possibility") {
 	GIVEN("Invalid operation") {
 		int const* invalid_result = nullptr;
 		WHEN("Result is created") {
@@ -120,14 +120,14 @@ SCENARIO("Core Result helper should be usable as failure with some possibility."
 	}
 }
 
-SCENARIO("Core Result failure appening should works.") {
+SCENARIO("Core Result failure appening should works") {
 	GIVEN("Invalid operation result") {
 		auto const result_test = failure<int, Failures::INVALID_UUID>();
 		WHEN("Result combine") {
 			Result<int, Failures::NOT_INSIDE, Failures::INVALID_UUID> const combined_result =
 				result_test.append_failures<Failures::NOT_INSIDE>();
 
-			THEN("The combined result object should be queried") {
+			THEN("The setted result object should be queried") {
 				REQUIRE(combined_result.is_failure() == true);
 				REQUIRE(combined_result.get_failure() == Failures::INVALID_UUID);
 			}
@@ -135,14 +135,14 @@ SCENARIO("Core Result failure appening should works.") {
 	}
 }
 
-SCENARIO("Core Result success setting should works.") {
+SCENARIO("Core Result success setting should works") {
 	GIVEN("Invalid operation result") {
 		auto const result_test = failure<int, Failures::INVALID_UUID>();
 		WHEN("Result combine") {
 			Result<int, Failures::NOT_INSIDE, Failures::INVALID_UUID> const combined_result =
 				result_test.append_failures<Failures::NOT_INSIDE>();
 
-			THEN("The combined result object should be queried") {
+			THEN("The setted result object should be queried") {
 				REQUIRE(combined_result.is_failure() == true);
 				REQUIRE(combined_result.get_failure() == Failures::INVALID_UUID);
 			}
@@ -150,35 +150,35 @@ SCENARIO("Core Result success setting should works.") {
 	}
 }
 
-scenario("core result failure removing should works with one id.") {
-	given("invalid operation result") {
+SCENARIO("core result failure removing should works with one failure id") {
+	GIVEN("invalid operation result") {
 		auto const result_test = failure<
 			int,
-			failures::invalid_uuid,
-			failures::invalid_uuid,
-			failures::not_inside,
-			failures::no_resources,
-			failures::unknown>();
-		when("result removing") {
-			auto const new_result = result_test.set_failures<failures::invalid_uuid>();
+			Failures::INVALID_UUID,
+			Failures::INVALID_UUID,
+			Failures::NOT_INSIDE,
+			Failures::NO_RESOURCES,
+			Failures::UNKNOWN>();
+		WHEN("result removing") {
+			auto const new_result = result_test.set_failures<Failures::INVALID_UUID>();
 
-			then("the combined result object should be coherent") {
-				info("result_test type is:" << typeid(result_test).name());
-				info("new_result tested type is :" << typeid(new_result.value()).name());
-				info(
+			THEN("the combined result object should be coherent") {
+				INFO("result_test type is:" << typeid(result_test).name());
+				INFO("new_result tested type is :" << typeid(new_result.value()).name());
+				INFO(
 					"new_result correct type is:"
-					<< typeid(result<int, failures::invalid_uuid>).name());
-				require(new_result.has_value() == true);
-				require(new_result.value().get_failure().value() == failures::invalid_uuid);
-				require(
+					<< typeid(Result<int, Failures::INVALID_UUID>).name());
+				REQUIRE(new_result.has_value() == true);
+				REQUIRE(new_result.value().get_failure().value() == Failures::INVALID_UUID);
+				REQUIRE(
 					typeid(decltype(new_result.value())).name()
-					== typeid(result<int, failures::invalid_uuid>).name());
+					== typeid(Result<int, Failures::INVALID_UUID>).name());
 			}
 		}
 	}
 }
 
-SCENARIO("Core Result failure removing should works with many Ids.") {
+SCENARIO("Core Result failure removing should works with many failures Ids") {
 	GIVEN("Invalid operation result") {
 		auto const result_test = failure<
 			int,
@@ -187,13 +187,13 @@ SCENARIO("Core Result failure removing should works with many Ids.") {
 			Failures::NOT_INSIDE,
 			Failures::NO_RESOURCES,
 			Failures::UNKNOWN>();
-		WHEN("Result removing") {
+		WHEN("Result setting") {
 			auto const new_result = result_test.set_failures<
 				Failures::INVALID_UUID,
 				Failures::NO_RESOURCES,
 				Failures::NOT_INSIDE>();
 
-			THEN("The combined result object should be coherent") {
+			THEN("The setted result object should be coherent") {
 				// INFO("result_test Failure Id is: " << result_test.get_failure().value());
 				INFO("result_test type is:" << typeid(result_test).name());
 				INFO("new_result tested type is :" << typeid(new_result.value()).name());
@@ -219,10 +219,50 @@ SCENARIO("Core Result failure removing should works with many Ids.") {
 	}
 }
 
+SCENARIO("Core Result failure removing should works with success") {
+	GIVEN("Valid operation result") {
+		constexpr int success_value = 2;
+		auto const result_test = success<
+			int,
+			Failures::INVALID_UUID,
+			Failures::NOT_INSIDE,
+			Failures::NO_RESOURCES,
+			Failures::UNKNOWN>(success_value);
+		WHEN("Result setting") {
+			auto const new_result = result_test.set_failures<
+				Failures::INVALID_UUID,
+				Failures::NO_RESOURCES,
+				Failures::NOT_INSIDE>();
+
+			THEN("The setted result object should be coherent") {
+				// INFO("result_test Failure Id is: " << result_test.get_failure().value());
+				INFO("result_test type is:" << typeid(result_test).name());
+				INFO("new_result tested type is :" << typeid(new_result.value()).name());
+				INFO(
+					"new_result correct type is:" << typeid(Result<
+																int,
+																Failures::INVALID_UUID,
+																Failures::NO_RESOURCES,
+																Failures::NOT_INSIDE>)
+														 .name());
+				REQUIRE(new_result.has_value() == true);
+				REQUIRE(new_result.value().get_success().value() == success_value);
+				REQUIRE(
+					typeid(decltype(new_result.value())).name()
+					== typeid(Result<
+								  int,
+								  Failures::INVALID_UUID,
+								  Failures::NO_RESOURCES,
+								  Failures::NOT_INSIDE>)
+						   .name());
+			}
+		}
+	}
+}
+
 SCENARIO(
 	"Core Result failure setting should fail if not at least one setted is equal to the base "
-	"Result "
-	"value.") {
+	"Result value") {
 	GIVEN("Invalid operation result") {
 		auto const result_test = failure<
 			int,
@@ -231,10 +271,10 @@ SCENARIO(
 			Failures::NOT_INSIDE,
 			Failures::NO_RESOURCES,
 			Failures::UNKNOWN>();
-		WHEN("Result removing") {
+		WHEN("Result setting") {
 			auto const new_result = result_test.set_failures<Failures::NOT_INSIDE>();
 
-			THEN("The combined result object should be coherent") {
+			THEN("The setted result object should be coherent") {
 				INFO("result_test type is:" << typeid(result_test).name());
 				INFO("new_result tested type is :" << typeid(new_result.value()).name());
 				REQUIRE(new_result.has_value() == false);
@@ -242,9 +282,51 @@ SCENARIO(
 		}
 	}
 }
-// TODO(Add tests for failures setting with success)
 // TODO(Add tests for success setting with failure)
-// TODO(Add tests for success setting with Succes type)
+SCENARIO("Core Result success setting should works with failure") {
+	GIVEN("Invalid operation result") {
+		auto const result_test = failure<
+			int,
+			Failures::INVALID_UUID,
+			Failures::INVALID_UUID,
+			Failures::NOT_INSIDE,
+			Failures::NO_RESOURCES,
+			Failures::UNKNOWN>();
+		WHEN("Result setting") {
+			auto const new_result = result_test.set_success<std::string>();
+
+			THEN("The setted result object should be coherent") {
+				INFO("result_test type is:" << typeid(result_test).name());
+				INFO("new_result tested type is :" << typeid(new_result.value()).name());
+				REQUIRE(new_result.has_value() == true);
+				REQUIRE(new_result.value().is_failure() == true);
+				REQUIRE(new_result.value().get_failure().value() == Failures::INVALID_UUID);
+			}
+		}
+	}
+}
+
+SCENARIO("Core Result success setting should works with success") {
+	GIVEN("Invalid operation result") {
+		constexpr int success_value = 2;
+		auto const result_test =
+			success<int, Failures::INVALID_UUID, Failures::NOT_INSIDE>(success_value);
+		WHEN("Result setting") {
+			const float new_success_value = 2.3;
+			std::optional<Result<float, Failures::INVALID_UUID, Failures::NOT_INSIDE>> const
+				new_result = result_test.set_success<float>(new_success_value);
+
+			THEN("The setted result object should be coherent") {
+				INFO("result_test type is:" << typeid(result_test).name());
+				INFO("new_result tested type is :" << typeid(new_result.value()).name());
+				REQUIRE(new_result.has_value() == true);
+				REQUIRE(new_result.value().is_success() == true);
+				REQUIRE(new_result.value().get_success().value() == new_success_value);
+			}
+		}
+	}
+}
+
 // TODO(Add tests for success setting with bad Succes type)
 // TODO(Add tests for success setting with Failure)
 // TODO(Add tests for success setting with Succes value)
