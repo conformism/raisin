29fddf00d4f26bb80e7a053a0c5c16b713e8122d
Dauliac <dauliac@pm.me>
2021-11-16 22:23:44 +0100

    [🐛] Add tests for domain::program

---
+++ test/unit/domain/program.cpp
@@ -0,0 +1,45 @@
+#include "domain/program.hpp"
+#include <catch2/catch_all.hpp>
+
+#include <any>
+#include <catch2/catch_test_macros.hpp>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "domain/cfg/cfg.hpp"
+
+using namespace domain::core::result;
+using namespace domain::core;
+
+SCENARIO("Program should works with UUID") {
+	GIVEN("Program ") {
+		domain::Program program = domain::Program();
+		AND_GIVEN("Exising cfg") {
+			auto builder = domain::cfg::Cfg::Builder();
+			auto const cfg = builder.build();
+			WHEN("The cfg is inserted into program") {
+				auto const uuid = cfg.get_uuid();
+				auto result = program.insert_cfg(cfg);
+				THEN("The result sould be success with good value") {
+					REQUIRE(result.is_success());
+					REQUIRE(cfg.get_uuid() == result.get_success().value()->get_uuid());
+					AND_WHEN("The cfg is queries from program") {
+						auto result = program.get_cfg_by_uuid(uuid);
+						THEN("The result should be successed with good value") {
+							REQUIRE(result.is_success());
+							REQUIRE(result.get_success().value()->get_uuid() == uuid);
+						}
+					}
+					AND_WHEN("The cfg is remove from program") {
+						auto result = program.remove_cfg(uuid);
+						THEN("The result should be successed") {
+							REQUIRE(result.is_success());
+						}
+					}
+				}
+			}
+		}
+	}
+}
