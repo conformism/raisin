461260b9522de9813c0ee63a49ffa547518f5b58
Dauliac <dauliac@pm.me>
2021-11-16 23:34:02 +0100

    [♻️] Implement  domain::Program into new repo design

--- src/infra/repositories/repository-in-memory.hpp
+++ src/infra/repositories/repository-in-memory.hpp
@@ -1,41 +1,40 @@
 #pragma once
 
 #include "domain/cfg/cfg.hpp"
+#include "domain/core/result.hpp"
 #include "domain/core/types.hpp"
-#include "domain/irepostory.hpp"
+#include "domain/irepository.hpp"
+#include "domain/program.hpp"
 #include <memory>
 
 namespace infra::repository {
 
 template<class Cfg, class CfgParserService>
-class RespositoryInMemory : public domain::IRespository<Cfg> {
+class RespositoryInMemory : public domain::IRespository<domain::Program> {
 public:
 	using Cfgs = typename domain::IRespository<Cfg>::Cfgs;
-	explicit RespositoryInMemory(std::unique_ptr<CfgParserService> cfg_parser_service)
+	explicit RespositoryInMemory(CfgParserService* cfg_parser_service)
 		: _cfg_parser_service(cfg_parser_service){};
 
-	auto save_cfgs(Cfgs cfgs) -> void override {
-		_cfgs = cfgs;
+	auto write(domain::Program program) -> void override {
+		_program = std::make_unique<domain::Program>(program);
 	}
 
-	auto update_cfg(Cfg cfg)
-		-> domain::Result<Cfg*, Failures::INVALID_UUID, Failures::NO_RESOURCES> override {
-		if (_cfgs.is_inside(cfg.get_uuid())) {
-			_cfgs.remove(cfg.get_uuid());
-		}
-		auto const result_cfg = _cfgs.insert(cfg.get_uuid(), cfg);
-		auto const result =
-			result_cfg.template set_failures<Failures::INVALID_UUID, Failures::NO_RESOURCES>();
-		return result.value();
-	};
+	[[nodiscard]] auto read() -> domain::Program* override {
+		return _program.get();
+	}
 
-	auto get_cfg_by_id(domain::core::Uuid cfg_uuid) -> domain::core::result::
-		Result<Cfg*, Failures::INVALID_UUID, Failures::NOT_INSIDE> override {
-		return _cfgs.at(cfg_uuid);
-	};
+	// NOLINTNEXTLINE(misc-unused-parameters)
+	[[nodiscard]] auto read_cfg_by_id(domain::core::Uuid uuid) -> domain::
+		Result<domain::Program*, Failures::INVALID_UUID, Failures::NO_RESOURCES> override {
+		return domain::core::result::
+			success<domain::Program*, Failures::INVALID_UUID, Failures::NO_RESOURCES>(
+				_program.get());
+	}
 
 private:
-	Cfgs _cfgs;
-	std::unique_ptr<CfgParserService> _cfg_parser_service;
+	std::unique_ptr<domain::Program> _program =
+		std::make_unique<domain::Program>(domain::Program());
+	CfgParserService* _cfg_parser_service;
 };
 }  // namespace infra::repository
