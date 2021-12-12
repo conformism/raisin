5b5a0f30f5a984e17efc00ab5883b36845d2f668
Dauliac <dauliac@pm.me>
2021-12-12 01:15:35 +0100

    [🐛] Clean, and add cfg setters

--- src/domain/cfg/block.hpp
+++ src/domain/cfg/block.hpp
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <algorithm>
-#include <memory>
-#include <string>
-#include <vector>
-
 #include "domain/core/types.hpp"
 #include "domain/core/aggregator.hpp"
 #include "domain/core/core.hpp"
@@ -19,15 +14,14 @@ public:
 
 	[[nodiscard]] auto is_entry() const -> bool;
 	[[nodiscard]] auto is_exit() const -> bool;
-	[[nodiscard]] auto get_text() const -> std::string;
+	[[nodiscard]] auto get_text() const -> std::string const*;
 	[[nodiscard]] auto set_text(std::string text) -> result::Result<std::string, Failures::CANT_HAVE_ZERO_LENGTH, Failures::ALREADY_SETTED>;
 	[[nodiscard]] auto get_successors() const -> Linkables const*;
 	[[nodiscard]] auto add_successors(Block const* block)
-		-> result::Result<Block*, Failures::INVALID_UUID, Failures::NO_RESOURCES, Failures::ALREADY_INSIDE>;
+		-> result::Result<Block*, Failures::NO_RESOURCES, Failures::ALREADY_INSIDE>;
 	[[nodiscard]] auto get_precedents() const -> Linkables const*;
 	[[nodiscard]] auto add_precedent(Block const* block)
-		-> result::Result<Block*, Failures::INVALID_UUID, Failures::NO_RESOURCES, Failures::ALREADY_INSIDE>;
-;
+		-> result::Result<Block*, Failures::NO_RESOURCES, Failures::ALREADY_INSIDE>;
 
 private:
 	std::string _text;
