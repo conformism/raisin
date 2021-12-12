8025302a99852594cb6cbecd058c6b4312c6ec05
Dauliac <dauliac@pm.me>
2021-12-12 01:27:17 +0100

    [🐛] Simplify domain/program methods

--- src/domain/program.hpp
+++ src/domain/program.hpp
@@ -10,7 +10,7 @@ public:
 	using Cfgs = core::Compositor<cfg::Cfg>;
 
 	[[nodiscard]] auto insert_cfg(cfg::Cfg cfg) -> core::result::
-		Result<cfg::Cfg*, Failures::INVALID_UUID, Failures::ALREADY_INSIDE>;
+		Result<cfg::Cfg*, Failures::ALREADY_INSIDE>;
 	[[nodiscard]] auto remove_cfg(core::Uuid uuid)
 		-> core::result::Result<bool, Failures::INVALID_UUID, Failures::NOT_INSIDE>;
 	[[nodiscard]] auto get_cfg_by_uuid(core::Uuid uuid)
