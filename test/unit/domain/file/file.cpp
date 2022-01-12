cb3144c80d70a0ea0d4735d39e88984eaf4f7d12
Dauliac <dauliac@pm.me>
2022-01-12 20:56:55 +0100

    [✅] (domain::file) Add tests for include logic

--- test/unit/domain/file/file.cpp
+++ test/unit/domain/file/file.cpp
@@ -5,15 +5,23 @@
 SCENARIO("File should be usable.") {
 	using namespace domain::file;
 	GIVEN("Datas about file") {
-		auto file_name = "file.txt";
+		auto const *const file_name = "file.txt";
+		auto const *const file_name_included = "file2.txt";
 		std::filesystem::path path(file_name);
+		std::filesystem::path path_included(file_name_included);
 		auto language = Language::CPP;
 
 		WHEN("File is created") {
-			File const file(path, language);
+			File file(path, language);
+			File file_included(path, language);
+			File file_not_included(path, Language::C);
+			file.include(&file_included);
+
 			THEN("The object should be usable.") {
 				REQUIRE(file.get_path().filename() == file_name);
 				REQUIRE(file.get_language() == language);
+				REQUIRE(file.get_include(file_included.get_uuid()) .is_success());
+				REQUIRE(file.get_include(file_not_included.get_uuid()).is_failure());
 			}
 		}
 	}
