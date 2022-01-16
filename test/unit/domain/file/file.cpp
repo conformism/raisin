862ea946449d2f1d335355db3215c9d6c124512c
Dauliac <dauliac@pm.me>
2022-01-16 19:47:30 +0100

    [✅]: file::Code Add tests

--- test/unit/domain/file/file.cpp
+++ test/unit/domain/file/file.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_all.hpp>
 
 #include "domain/file/file.hpp"
+#include "domain/file/code.hpp"
 
 SCENARIO("File should be usable.") {
 	using namespace domain::file;
@@ -22,6 +23,27 @@ SCENARIO("File should be usable.") {
 				REQUIRE(file.get_language() == language);
 				REQUIRE(file.get_include(file_included.get_uuid()) .is_success());
 				REQUIRE(file.get_include(file_not_included.get_uuid()).is_failure());
+			}
+
+			WHEN("Code is created") {
+				constexpr auto start_line = 0;
+				constexpr auto end_line = 20000;
+				constexpr auto start_char = 0;
+				constexpr auto end_char = 50000;
+
+				Code const code = Code(file.get_uuid(), start_line, end_line, start_char, end_char);
+				Code const same_code = Code(file.get_uuid(), start_line, end_line, start_char, end_char);
+				Code const different_code = Code(file.get_uuid(), start_line, end_line, 0, 17);
+
+				THEN("The code object be usable and comparable") {
+					REQUIRE(code == same_code);
+					REQUIRE(code != different_code);
+
+					REQUIRE(code.get_start_line() == start_line);
+					REQUIRE(code.get_end_line() == end_line);
+					REQUIRE(code.get_start_char() == start_char);
+					REQUIRE(code.get_end_char() == end_char);
+				}
 			}
 		}
 	}
